cac252c4e339150ae2b85021e6506ac61b8c0830
Darcy <geodarcy@gmail.com>
2014-11-25 15:41:45 -0700

    Read GeoJSON from text file Style data Fade old data Create popup Edit existing data and add new data Todo: edit properties of added data Todo: style added data Todo: read data from database Todo: save changes back to the database  Changes to be committed: 	modified:   css/winterbiking.css 	modified:   data/winterbiking.geojson 	modified:   data/winterbiking.js 	modified:   home.html 	modified:   js/featureFunctions.js

--- js/featureFunctions.js
+++ js/featureFunctions.js
@@ -13,27 +13,34 @@ var hazardIcon = L.icon({
 });
 
 function initBikeJson (feature, layer) {
-  styleMarkers(feature, layer);
-  if (feature.geometry.type == "LineString")
-    fadeOldMarkers(feature, layer);
+  var tempLayer = layer;
+  tempLayer.feature = layer.feature;
+  var popupText = "Condition: <b>" + layer.feature.properties.Condition + "</b><br>";
+  if (layer.feature.properties.Comment)
+    popupText += "Comment: <b>" + layer.feature.properties.Comment + "</b><br>";
+  if (layer.feature.properties.created_user)
+    popupText += "Created By: <b>" + layer.feature.properties.created_user + "</b><br>";
+  popupText += "Created On: <b>" + layer.feature.properties.last_edited_date + "</b>";
+  tempLayer.bindPopup(popupText);
+  drawnItems.addLayer(tempLayer);
 }
 
-function styleMarkers (feature, layer) {
-//  console.log(feature);
-  if (feature.geometry.type == "LineString" & feature.properties.Condition == "Poor")
-    layer.setStyle({color: "#ff0000"});
-  if (feature.geometry.type == "LineString" & feature.properties.Condition == "Fair")
-    layer.setStyle({color: "#f7f619"});
-  if (feature.geometry.type == "LineString" & feature.properties.Condition == "Good")
-    layer.setStyle({color: "green", opacity: 1});
-  if (feature.geometry.type == "Point" & feature.properties.Condition == "Hazard")
-    layer.setIcon(hazardIcon);
-  if (feature.geometry.type == "Point" & feature.properties.Condition == "Caution")
-    layer.setIcon(cautionIcon);
+function styleMarkers (layer) {
+//  console.log(layer);
+  if (layer.layer.feature.geometry.type == "LineString" & layer.layer.feature.properties.Condition == "Poor")
+    layer.layer.setStyle({color: "#ff0000"});
+  if (layer.layer.feature.geometry.type == "LineString" & layer.layer.feature.properties.Condition == "Fair")
+    layer.layer.setStyle({color: "#f7f619"});
+  if (layer.layer.feature.geometry.type == "LineString" & layer.layer.feature.properties.Condition == "Good")
+    layer.layer.setStyle({color: "green"});
+  if (layer.layer.feature.geometry.type == "Point" & layer.layer.feature.properties.Condition == "Hazard")
+    layer.layer.setIcon(hazardIcon);
+  if (layer.layer.feature.geometry.type == "Point" & layer.layer.feature.properties.Condition == "Caution")
+    layer.layer.setIcon(cautionIcon);
 }
 
-function fadeOldMarkers (feature, layer) {
-  var editedDate = new Date(feature.properties.last_edited_date);
+function fadeOldLines (layer) {
+  var editedDate = new Date(layer.feature.properties.last_edited_date);
   var today = new Date();
   var timeDiff = Math.abs(editedDate.getTime() - today.getTime());
   var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
@@ -46,4 +53,20 @@ function fadeOldMarkers (feature, layer) {
     layer.setStyle({opacity: 0.25});
   else
     layer.setStyle({opacity: 0.1});
+}
+
+function fadeOldMarkers (layer) {
+  var editedDate = new Date(layer.feature.properties.last_edited_date);
+  var today = new Date();
+  var timeDiff = Math.abs(editedDate.getTime() - today.getTime());
+  var daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+//  console.log(daysDiff);
+  if (daysDiff <= 7)
+    layer.setOpacity(0.1);
+  else if (daysDiff <= 14)
+    layer.setOpacity(0.5);
+  else if (daysDiff <= 21)
+    layer.setOpacity(0.25);
+  else
+    layer.setOpacity(0.1);
 }
