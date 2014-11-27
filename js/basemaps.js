f5c40611ea14aafa2c7866ff9b21aa9c7beb20bd
geodarcy <geodarcy@gmail.com>
2014-11-26 22:17:45 -0700

    Change basemaps  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   home.html
    	modified:   js/basemaps.js

--- js/basemaps.js
+++ js/basemaps.js
@@ -1,11 +1,14 @@
-var osmUrl='http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
-var topoUrl='http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}';
-var topoAttrib='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+var stamenTonerLiteUrl = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
+var stamenTonerLiteAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+var cartoDBUrl='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+var cartoDBAttrib='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 var imageryUrl='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
 var imageryAttrib='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
-var topo = new L.TileLayer(topoUrl, {attribution: topoAttrib});
+var stamenTonerLite = new L.TileLayer(stamenTonerLiteUrl, {attribution: stamenTonerLiteAttrib});
+var cartoDB = new L.TileLayer(cartoDBUrl, {attribution: cartoDBAttrib});
 var imagery = new L.TileLayer(imageryUrl, {attribution: imageryAttrib});
 var openPrecipitation = L.tileLayer('http://{s}.tile.openweathermap.org/map/precipitation_cls/{z}/{x}/{y}.png', {
 attribution: 'Map data &copy; <a href="http://openweathermap.org">OpenWeatherMap</a>',
@@ -18,8 +21,9 @@ opacity: 0.5
 
 var baseMaps = {
 	"Satellite": imagery,
-	"Topographic": topo,
-	"Open Street Map": osm
+	"CartoDB": cartoDB,
+	"Open Street Map": osm,
+	"Stamen.TonerLite": stamenTonerLite
 };
 var overlays = {
 	"Temperature": openTemperature,
