5be6445519607e0bc140f7cd702e4140733f56ec
Darcy <geodarcy@gmail.com>
2014-12-03 15:16:21 -0700

    read data from CartoDB add POST for CartoDB Better to use PHP to interact with CartoDB  On branch readCartoDBJSONAgain  Changes to be committed: 	modified:   home.html 	modified:   js/featureFunctions.js

--- js/featureFunctions.js
+++ js/featureFunctions.js
@@ -16,13 +16,19 @@ var currentLayer, jsonData;
 
 function readData() {
   try {
-		$.get("./data/winterbiking.geojson", function(data, status) {
-			bikeEdits = JSON.parse(data);
-			bikeJson = L.geoJson(bikeEdits, {
-				onEachFeature: initBikeJson
-			});
-		});
-	}
+		var url = "https://geodarcy.cartodb.com/api/v2/sql?format=geojson&q=SELECT cartodb_id,the_geom,condition,created_user,last_edited_date,comment FROM winterbiking";
+    $.getJSON(url, function(data) {
+      geojsonLayer = L.geoJson(data, {
+        onEachFeature: function (feature, layer) {
+          layer.cartodb_id=feature.properties.cartodb_id;
+          feature.properties.last_edited_date = new Date(feature.properties.last_edited_date);
+          drawnItems.addLayer(layer);
+        }
+      });
+      map.addLayer(drawnItems);
+      updateAllMarkers();
+    });
+  }
 	catch(err) {
 	  console.log("There are no data yet.");
 	}
@@ -34,14 +40,14 @@ function writeData(callbackFunction) {
   callbackFunction();
 }
 
-function changeCondition(value) {
-  currentLayer.feature.properties.Condition = value;
+function changecondition(value) {
+  currentLayer.feature.properties.condition = value;
   currentLayer.feature.properties.last_edited_date = new Date();
   updateAllMarkers();
 }
 
-function changeComment(value) {
-  currentLayer.feature.properties.Comment = value;
+function changecomment(value) {
+  currentLayer.feature.properties.comment = value;
   currentLayer.feature.properties.last_edited_date = new Date();
   updateAllMarkers();
 }
@@ -55,47 +61,47 @@ function changeCreator(value) {
 function updateAllMarkers() {
   drawnItems.eachLayer(function(layer) {
     styleMarkers(layer);
-    if (layer.feature.geometry.type == "LineString")
+    if (layer.feature.geometry.type == "MultiLineString")
       fadeOldLines(layer);
 		if (layer.feature.geometry.type == "Point")
 			fadeOldPoints(layer);
     var popupText = createPopup(layer);
     layer.bindPopup(popupText);
   });
-  jsonData = JSON.stringify(drawnItems.toGeoJSON());
+/*  jsonData = JSON.stringify(drawnItems.toGeoJSON());
 //  console.log(jsonData);
   writeData(function() {
 		$.post("./php/writewbjson.php", 'data='+jsonData, function(){drawnItems.clearLayers();readData();});
-	});
+	});*/
 }
 
 function createPopup(layer) {
   var myLayer = layer;
-  var popupText = "Condition: <select id='condition' onchange='changeCondition(this.value)'>";
-  if (!layer.feature.properties.Condition)
+  var popupText = "Condition: <select id='condition' onchange='changecondition(this.value)'>";
+  if (!layer.feature.properties.condition)
     popupText += "<option disabled selected='selected'>Select a riding condition</option><option";
   else
     popupText += "<option";
-  if (layer.feature.geometry.type == "LineString") {
-    if (layer.feature.properties.Condition == 'Good')
+  if (layer.feature.geometry.type == "MultiLineString") {
+    if (layer.feature.properties.condition == 'Good')
       popupText += " selected='selected' ";
     popupText += ">Good</option><option";
-    if (layer.feature.properties.Condition == 'Fair')
+    if (layer.feature.properties.condition == 'Fair')
       popupText += " selected='selected' ";
     popupText += ">Fair</option><option";
-    if (layer.feature.properties.Condition == 'Poor')
+    if (layer.feature.properties.condition == 'Poor')
       popupText += " selected='selected' ";
     popupText += ">Poor</option></select></br>";
   }
   if (layer.feature.geometry.type == "Point") {
-    if (layer.feature.properties.Condition == 'Caution')
+    if (layer.feature.properties.condition == 'Caution')
       popupText += " selected='selected' ";
     popupText += ">Caution</option><option";
-    if (layer.feature.properties.Condition == 'Hazard')
+    if (layer.feature.properties.condition == 'Hazard')
       popupText += " selected='selected' ";
     popupText += ">Hazard</option></select></br>";
   }
-  popupText += "Comment: <textarea onchange='changeComment(this.value)' tabindex='1'>" + layer.feature.properties.Comment + "</textarea><br>";
+  popupText += "Comment: <textarea onchange='changecomment(this.value)' tabindex='1'>" + layer.feature.properties.comment + "</textarea><br>";
   popupText += "Created By: <textarea onchange='changeCreator(this.value)' tabindex='2'>" + layer.feature.properties.created_user + "</textarea><br>";
   popupText += "Created On: <b>" + layer.feature.properties.last_edited_date.toDateString() + "</b>";
   return popupText;
@@ -110,7 +116,7 @@ function initBikeJson (feature, layer) {
   tempLayer.bindPopup(popupText);
   tempLayer.on('popupopen', function(){currentLayer = tempLayer;});
   styleMarkers(tempLayer);
-  if (tempLayer.feature.geometry.type == "LineString")
+  if (tempLayer.feature.geometry.type == "MultiLineString")
     fadeOldLines(tempLayer);
   if (tempLayer.feature.geometry.type == "Point")
     fadeOldPoints(tempLayer);
@@ -120,15 +126,15 @@ function initBikeJson (feature, layer) {
 function styleMarkers (layer) {
 //  console.log(layer);
   if (layer.feature) {
-    if (layer.feature.geometry.type == "LineString" & layer.feature.properties.Condition == "Poor")
+    if (layer.feature.geometry.type == "MultiLineString" & layer.feature.properties.condition == "Poor")
       layer.setStyle({color: "#ff0000"});
-    if (layer.feature.geometry.type == "LineString" & layer.feature.properties.Condition == "Fair")
+    if (layer.feature.geometry.type == "MultiLineString" & layer.feature.properties.condition == "Fair")
       layer.setStyle({color: "#f7f619"});
-    if (layer.feature.geometry.type == "LineString" & layer.feature.properties.Condition == "Good")
+    if (layer.feature.geometry.type == "MultiLineString" & layer.feature.properties.condition == "Good")
       layer.setStyle({color: "green"});
-    if (layer.feature.geometry.type == "Point" & layer.feature.properties.Condition == "Hazard")
+    if (layer.feature.geometry.type == "Point" & layer.feature.properties.condition == "Hazard")
       layer.setIcon(hazardIcon);
-    if (layer.feature.geometry.type == "Point" & layer.feature.properties.Condition == "Caution")
+    if (layer.feature.geometry.type == "Point" & layer.feature.properties.condition == "Caution")
       layer.setIcon(cautionIcon);
   }
 }
@@ -188,4 +194,61 @@ function geolocateMe() {
   map.panTo([pos.lat, pos.lng]);
 	geolocateMarker = new L.marker([pos.lat, pos.lng]).addTo(map);
 	geolocateMarker.bindPopup('You may be here').openPopup();
+}
+
+function persistOnCartoDB(action,layers) {
+  console.log(action);
+  console.log(layers);
+  var cartodb_ids=[];
+  var geojsons=[];
+
+  switch(action) {
+    case "UPDATE":
+      if (layers.getLayers().length<1) return;
+
+      layers.eachLayer(function(layer) {
+        cartodb_ids.push(layer.cartodb_id);
+        geojsons.push("'"+JSON.stringify(layer.toGeoJSON())+"'");
+      });
+      break;
+
+    case "INSERT":
+      cartodb_ids.push(-1);
+      geojsons.push("'"+JSON.stringify(layers.toGeoJSON())+"'");
+      break;
+
+    case "DELETE":
+      layers.eachLayer(function(layer) {
+        cartodb_ids.push(layer.cartodb_id);
+        geojsons.push("''");
+      });
+      break;
+  }
+
+  var sql = "SELECT geodarcy_upsert_leaflet_data(ARRAY[";
+  sql += cartodb_ids.join(",");
+  sql += "],ARRAY[";
+  sql += geojsons.join(",");
+  sql += "]);";
+
+  console.log("persisting... " + sql  );
+  $.ajax({
+    type: 'POST',
+    url: 'https://geodarcy.cartodb.com/api/v2/sql',
+    crossDomain: true,
+    data: {"q":sql},
+    dataType: 'json',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Authorization", "Bearer $token")
+    },
+    success: function(responseData, textStatus, jqXHR) {
+      console.log("Data saved");
+
+      if (action=="INSERT")
+        layers.cartodb_id = responseData.rows[0].cartodb_id;
+    },
+    error: function (responseData, textStatus, errorThrown) {
+      console.log("Problem saving the data");
+    }
+  });
 }
