b3a0e2092e058e4baddfaa820134cc5bef474b1c
geodarcy <geodarcy@gmail.com>
2015-06-22 21:49:43 -0600

    Add mode/year filter  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   bikepedcollisions1/css/winterbiking.css
    	modified:   bikepedcollisions1/home.html
    	modified:   bikepedcollisions1/js/functions.js

--- bikepedcollisions1/js/functions.js
+++ bikepedcollisions1/js/functions.js
@@ -14,6 +14,7 @@ function loadAllData()
   $.getJSON(url, function(data) {
     var myJson = L.geoJson(data, {
       onEachFeature: function (feature, layer) {
+        var modeYear = feature.properties['Mode'] + feature.properties['Year'];
         var popupText = feature.properties['Collision Location Name'];
         popupText += "<br/><strong>Roadway Portion:</strong> " + feature.properties['Roadway Portion'];
         popupText += "<br/><strong>Year:</strong> " + feature.properties['Year'];
@@ -21,11 +22,41 @@ function loadAllData()
         popupText += "<br/><strong>Count:</strong> " + feature.properties['Count'];
         layer.bindPopup(popupText);
         if(feature.properties['Mode'] == 'Bike')
+        {
           layer.setIcon(bikeIcon);
+        }
         else
+        {
           layer.setIcon(pedIcon);
+        }
+        if (! layerDict[modeYear])
+          layerDict[modeYear] = [layer];
+        else
+        layerDict[modeYear].push(layer);
         markers.addLayer(layer);
       }
     });
   });
+}
+
+function filterData()
+{
+  if (document.getElementById('modeMenu').value == 'all')
+    var modes = ['Bike', 'Ped'];
+  else
+    var modes = [document.getElementById('modeMenu').value];
+  var years = [];
+  for (i=0; i<document.getElementsByName('checkYear').length; i++)
+  {
+    if (document.getElementsByName('checkYear')[i].checked)
+      years.push(document.getElementsByName('checkYear')[i].value);
+  }
+  markers.clearLayers();
+  for (i=0; i<modes.length; i++)
+  {
+    for (j=0; j<years.length; j++)
+    {
+      markers.addLayers(layerDict[modes[i] + years[j]]);
+    }
+  }
 }
