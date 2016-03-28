6dd863c6531d0b203c6197216acfe52e323cbbc3
geodarcy <geodarcy@gmail.com>
2016-03-28 11:40:54 -0600

    Revert the next Paths For People map back to the original basemaps  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	modified:   pfp2/js/basemaps.js

--- pfp2/js/basemaps.js
+++ pfp2/js/basemaps.js
@@ -1,8 +1,8 @@
-var osmUrl='http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
-var osmAttrib='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>';
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Tiles courtesy of <a href="http://hot.openstreetmap.org/" target="_blank">Humanitarian OpenStreetMap Team</a>';
 var stamenTonerLiteUrl = 'http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png'
 var stamenTonerLiteAttrib = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-var cartoDBUrl='http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+var cartoDBUrl='http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
 var cartoDBAttrib='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
 var imageryUrl='http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
 var imageryAttrib='Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
